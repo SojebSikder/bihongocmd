f01c9ea5f16ec487a793d5d0f43d4a2635c1ed35
sojebsikder <sojebsikder@gmail.com>
2020-07-20 17:03:46 +0600

    uploade

---
+++ phpcmd/CreateProject.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+
+namespace phpcmd
+{
+   static class CreateProject
+    {
+               static string addressController = "app/controllers/";
+               static string addressModel = "app/models/";
+
+                public static void Create(string projectName,string address)
+                {
+                    if (!Directory.Exists(address))
+                    {
+                        Directory.CreateDirectory("app/controllers/");
+                        Directory.CreateDirectory("app/models/");
+                        Directory.CreateDirectory("app/views/");
+
+                        Directory.CreateDirectory("system/libs/");
+                        Directory.CreateDirectory("system/libs/");
+                        Directory.CreateDirectory("system/libs/");
+                        Directory.CreateDirectory("system/libs/");
+
+                        Directory.CreateDirectory("config/");
+
+                        Directory.CreateDirectory("resources/css/");
+                        Directory.CreateDirectory("resources/js/");
+                        
+
+
+
+                        CreateLibrary(address);
+                        CreateController("IndexController", addressController, true);
+                        CreateModel("IndexModel", addressModel, true);
+
+                    }
+
+                }
+
+                public static void CreateController(string controllerName,  string address, bool inf = false)
+                {
+
+                    if (!Directory.Exists(address))
+                    {
+                        Directory.CreateDirectory(address);
+                    }
+
+                    using (Stream s = File.Open(address + controllerName + ".php", FileMode.Create))
+                    using (StreamWriter sw = new StreamWriter(s))
+                    {
+                        string view = "\"home\"";
+                        sw.Write(@"<?php 
+
+class " + controllerName + @" extends Controller{
+	public function __construct(){
+	 	parent::__construct();
+	}
+
+	public function home(){
+	 	$this->load->view(" + view + @");
+	}
+}
+?>");
+                        if(inf  == false){
+                        
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("\nController Created Successfully...");
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                        }
+
+                    }
+                }//End CreateController
+
+                public static void CreateModel(string modelName, string address, bool inf = false)
+                {
+
+                    if (!Directory.Exists(address))
+                    {
+                        Directory.CreateDirectory(address);
+                    }
+
+                    using (Stream s = File.Open(address + modelName + ".php", FileMode.Create))
+                    using (StreamWriter sw = new StreamWriter(s))
+                    {
+                        sw.Write(@"<?php
+
+class " + modelName + @" extends Model{
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function dataList($query){
+        return $this->db->select($query);
+    }
+}
+
+
+?>");
+                        if (inf == false)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("\nModel Created Successfully...");
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                        }
+
+                    }
+                }//End Create Model
+
+
+                public static void CreateLibrary(string address)
+                {
+                    string Controller = GetFile(@"C:\sojeb\file\system\libs\Controller.php"); //end controller code
+
+                    string Model = GetFile(@"C:\sojeb\file\system\libs\Model.php"); //end model code
+                    string Database = GetFile(@"C:\sojeb\file\system\libs\Database.php"); //end database code
+                    string Load = GetFile(@"C:\sojeb\file\system\libs\Load.php"); //end load code
+                    string config = GetFile(@"C:\sojeb\file\config\dbconfig.php"); //end config code
+                    string index = GetFile(@"C:\sojeb\file\index.php"); //end index code
+                    string htaccess = GetFile(@"C:\sojeb\file\.htaccess"); //end htaccess code
+                    string homepage = GetFile(@"C:\sojeb\file\app\views\home.php"); // homepage code
+
+
+                    CreateFile("Controller","system/libs/",Controller);
+                    CreateFile("Model", "system/libs/", Model);
+                    CreateFile("Load", "system/libs/", Load);
+                    CreateFile("Database", "system/libs/", Database);
+                    CreateFile("dbconfig", "config/", config);
+                    CreateFile("index", "", index);
+                    CreateFile("home","app/views/",homepage);
+                    CreatehtaFile(".htaccess", "", htaccess);
+                }
+                public static void CreateFile(string name, string address, string content)
+                {
+                    using (Stream s = File.Open(address + name + ".php", FileMode.Create))
+                    using (StreamWriter sw = new StreamWriter(s))
+                    {
+                        sw.Write(content);
+
+                    }
+                }
+
+                public static void CreatehtaFile(string name, string address, string content)
+                {
+                    using (Stream s = File.Open(address + name, FileMode.Create))
+                    using (StreamWriter sw = new StreamWriter(s))
+                    {
+                        sw.Write(content);
+
+                    }
+                }
+
+                public static string GetFile(string url)
+                {
+                    WebRequest request = WebRequest.Create(url);
+                    //execute the request
+                    WebResponse response = request.GetResponse();
+                    //read data via response stream
+                    Stream resStream = response.GetResponseStream();
+                    string tempString = null;
+                    int count = 0;
+                    StringBuilder sb = new StringBuilder();
+                    byte[] buf = new byte[60];
+
+                    do
+                    {
+                        count = resStream.Read(buf, 0, buf.Length);
+                        if (count != 0)
+                        {
+                            tempString = Encoding.ASCII.GetString(buf, 0, count);
+                            sb.Append(tempString);
+                        }
+
+                    } while (count > 0);
+
+                    return sb.ToString();
+                }
+
+   }
+}
---
+++ phpcmd/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace phpcmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                string addressController = "app/controllers/";
+                string addressModel = "app/models/";
+
+                string[] cmd = {
+                                    "create controller = make:controller-<name of controller>",
+                                    "create model = make:model-<name of model>"
+                                };
+
+                foreach (string arg in args)
+                {
+
+                    //spliting string with separator
+                    char[] separatorCmd = { ':','-' };
+                    string[] spCmd = arg.Split(separatorCmd, StringSplitOptions.RemoveEmptyEntries);
+
+                    char[] separatorSpace = { '-',':' };
+                    string[] spSpace = arg.Split(separatorSpace, StringSplitOptions.RemoveEmptyEntries);
+
+                    //assaign to string variable
+                    string Maincmd = spCmd[0];
+                    string Typecmd = spCmd[1];
+                    string Namecmd = spSpace[2];
+
+                    //Start Command
+                    if (Maincmd == "help")
+                    {
+                        foreach (var item in cmd)
+                        {
+                            Console.Write(item + "\n");
+                        }
+                    }
+
+                    //Creating framework project
+                    if(Maincmd == "create"){ //creating command name
+                        Console.Write("Creating ");
+                        if(Typecmd == "project"){
+                            Console.Write("Project ");
+
+                            if(Namecmd != null){
+                                Console.Write(Namecmd);
+                                //Create project
+                                CreateProject.Create(Namecmd, Namecmd);
+
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.WriteLine("\nProject Created Successfully...");
+                                Console.ForegroundColor = ConsoleColor.Gray;
+                                   
+                                }
+
+                            }
+                        }
+
+                    
+
+
+                    if (Maincmd == "make")
+                    { //making command name
+
+                        Console.Write("Creating ");
+
+                        if (Typecmd == "controller")
+                        {  //command name
+
+                            Console.Write("Controller ");
+
+                            if (Namecmd != null) //controller name
+                            {
+                                Console.Write(Namecmd);
+                                CreateProject.CreateController(Namecmd,addressController);
+
+                            }
+                        } // controller
+
+
+                        if (Typecmd == "model"){  //command name
+                            Console.Write("Model ");
+
+                            if (Namecmd != null)//model name
+                            {
+                                Console.Write(Namecmd);
+                                CreateProject.CreateModel(Namecmd,addressModel);
+                            }
+                        } // model
+                    }
+
+                }
+                //Console.ReadLine();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }       
+            
+        }
+    }
+}
